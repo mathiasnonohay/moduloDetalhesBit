e69d98bab1ea3574c4d327de0dc1e80c1adbbfd6
Mathias Nonohay <mathias12300@gmail.com>
2021-01-21 14:47:03 -0300

    primeiro commit

---
+++ Example/Pods/Target Support Files/Pods-detalhesBit_Example/Pods-detalhesBit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_detalhesBit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_detalhesBit_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-detalhesBit_Tests/Pods-detalhesBit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_detalhesBit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_detalhesBit_Tests
+@end
