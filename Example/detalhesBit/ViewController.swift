2f00ab9956c40b70918c649f046f6d472261fb06
Mathias Nonohay <mathias12300@gmail.com>
2021-01-21 14:37:13 -0300

    Initial commit

---
+++ Example/detalhesBit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  detalhesBit
+//
+//  Created by mathiasnonohay on 01/21/2021.
+//  Copyright (c) 2021 mathiasnonohay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
