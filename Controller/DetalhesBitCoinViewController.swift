739f7feefdc994d22f19a9376039b7d4ee7f08b0
Mathias Nonohay <mathias12300@gmail.com>
2021-01-22 11:38:05 -0300

    tela detalhes 90% pronta

---
+++ Controller/DetalhesBitCoinViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetalhesBitCoinViewController.swift
+//  detalhesBit
+//
+//  Created by Mathias Almeida Nonohay on 1/21/21.
+//
+
+import UIKit
+
+public class DetalhesBitCoinViewController: UIViewController {
+
+    private var valor
+    
+    init(<#parameters#>) {
+        <#statements#>
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
